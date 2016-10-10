5b753197a1052f37a2e3f10ef6adffe85a088dc3
Anoop Ismail <pi.anoop@gmail.com>
2016-10-10 11:00:27 +0400

    AI Popups
    Custom popup designs

---
+++ scripts/AI-Script.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    // AI Modal Popup
+
+    $("a, button, div").click(function (e) {
+        var getTargetID = $(this).data("target");
+        var getDismiss = $(this).data("dismiss");
+
+        if (getTargetID != undefined) {
+            $(getTargetID).fadeIn(function () {
+                $(getTargetID).removeClass();
+                $(getTargetID).addClass('popup-holder-show');
+            });
+        }
+
+        if (getDismiss != undefined) {
+            $(getDismiss).fadeOut(function (event) {
+                $(getDismiss).removeClass();
+                $(getDismiss).addClass('popup-holder');
+            });
+        }
+
+
+    });
+});
